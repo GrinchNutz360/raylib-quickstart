20b1441cb19bdd6674f044d30d5e9f4fe979d24f
Tristin <TCturtle16@outlook.com>
2026-01-10 13:55:19 -0700

    Changed it to have game of life features that can be randomized with the press of the space bar

--- src/main.cpp
+++ src/main.cpp
@@ -68,6 +68,9 @@ int CountLiveNeighbors(int x, int y, const Cells& cells)
 int main ()
 {
 
+	// randomize seed value using the system clock
+	SetRandomSeed((unsigned int)time_t(NULL));
+
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
@@ -76,6 +79,9 @@ int main ()
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
+
+	// set the target fps, set to lower value to make generation slower(30, 20 or 10)
+	SetTargetFPS(10);
 
 	Cells currentGeneration(gridWidth * gridHeight);
 	Cells nextGeneration(gridWidth * gridHeight);
@@ -87,6 +93,34 @@ int main ()
 	while (!WindowShouldClose())		// run the loop until the user presses ESCAPE or presses the Close button on the window
 	{
 		std::fill(nextGeneration.begin(), nextGeneration.end(), false);
+
+		// randomize cells if spacebar is pressed
+			if (GetKeyPressed() == KEY_SPACE) {
+				// randomize current generation
+				RandomizeCells(currentGeneration);
+			}
+
+			// update next generation cells from current generation
+				for (int y = 1; y < gridHeight - 1; y++) {
+					for (int x = 1; x < gridWidth - 1; x++) {
+						int count = CountLiveNeighbors(x, y, currentGeneration);
+						bool isAlive = ReadCell(x, y, currentGeneration);
+						// Conway's Game of Life rules:
+						// 1. Live cell with 2-3 neighbors survives
+						// 2. Dead cell with exactly 3 neighbors becomes alive
+						// 3. All other cells die or stay dead
+						if (isAlive) {
+							// write true to next generation if survives
+							if (count > 1 && count <= 3) {
+								WriteCell(x, y, 1, nextGeneration);
+							}
+						}
+						else {
+							// write true to next generation if becomes alive
+							if (count == 3) WriteCell(x, y, 1, nextGeneration);
+						}
+					}
+				}
 
 		// drawing
 		BeginDrawing();
@@ -111,6 +145,8 @@ int main ()
 		
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
+
+		currentGeneration = nextGeneration;
 	}
 
 	// cleanup
